4c8073d36aa6c3537bca0a11d146c4498330f853
prakashabhishant <prakashabhishant@gmail.com>
2018-05-09 11:28:11 +0530

    Added the data json and dictionary python file

---
+++ Dictionary.py
@@ -0,0 +1,12 @@
+# importing the data from the json file
+
+import json 
+data = json.load(open("data.json"));
+
+
+def translate(word):
+	return data[word];
+
+word = input("Enter Word: ");
+
+print(translate(word));
